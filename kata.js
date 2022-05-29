d93b3adb66aab3a269cb034a410dc6461510eeeb
Nate Grossmann <nategr54@gmail.com>
2022-05-29 12:25:13 -0600

    added hula hoop counter kata

--- kata.js
+++ kata.js
@@ -118,3 +118,18 @@ function countSheep(arrOfSheep) {
   const result = arrOfSheep.filter(x=> x === true)
   return result
 }
+
+//Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+  //If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+  //If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+function hoopCount(n) {
+  return (n < 10) ? 'Keep at it until you get it' : "Great, now move on to tricks"
+}
+  //or
+function hoopCount(n) {
+  const result1 = "Great now move on to tricks"
+  const result2 = "Keep at it until you get it"
+  if(n <=10) {
+    return result1
+  } else return result2
+}
