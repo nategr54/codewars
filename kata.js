62af05479e0dfaba6e1058c03438415c6e606573
Nate Grossmann <nategr54@gmail.com>
2022-07-15 22:10:08 -0600

    used parseInt method

--- kata.js
+++ kata.js
@@ -181,3 +181,8 @@ function squareSum(numbers){
   }, 0)
   return sum
   }
+
+//Write a program that returns the girl's age (0-9) as an integer.
+function getAge(inputString){
+  return parseInt(inputString.charAt(0)); 
+}
