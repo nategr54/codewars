f97f8a030a466f8e2284103a4a3f2fae9d61b344
Nate Grossmann <nategr54@gmail.com>
2022-07-18 12:12:12 -0600

    first 7kyu categorize members given an array using map method

--- kata.js
+++ kata.js
@@ -218,3 +218,13 @@ describe( "stringToNumber", function(){
   it( "should work for the examples" , function(){
     Test.assertEquals(stringToNumber("1234"),1234)
     Test.assertEquals(stringToNumber("605"), 605)
+
+
+//Determine if croquet members will be sorted in to the senior or open categories. Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+//Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+//Senior === age > 54 && handicap > 7
+
+function openOrSenior(data) {
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open')
+
+}
